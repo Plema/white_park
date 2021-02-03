42387c8c5a746ded6fd32cc195a11c9f31cecf15
Anton Milasenko <48668353+Miant1@users.noreply.github.com>
2021-02-03 22:14:57 +0200

    last changes from 3.02

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-$()  
+$(document).ready(function() {
+
+    // Show/hide nav
+    $('.nav__toggle').on("click", function(e) {
+        e.preventDefault();
+
+        $(this).toggleClass('active');
+        if ($('.nav__toggle').hasClass('active')) {
+            $('.nav__toggle_btn').find('i').removeClass('icon-menu');
+            $('.nav__toggle_btn').find('i').addClass('icon-close');
+        } else{
+            $('.nav__toggle_btn').find('i').removeClass('icon-close');
+            $('.nav__toggle_btn').find('i').addClass('icon-menu');
+        }
+        $('.header__info').slideToggle(500).toggleClass('active');
+
+    });
+});
