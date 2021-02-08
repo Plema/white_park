3ae67420ff1c55c5a509996d7fdb7f5746308c49
Anton Milasenko <48668353+Miant1@users.noreply.github.com>
2021-02-08 19:43:31 +0200

    last changes from 8.02

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,11 +28,37 @@ $(document).ready(function() {
     //show
     $('.intro__play').click(function() {
         $('.intro__play_outline').toggleClass('active');
-        $('.video__popup').toggle();
+        $('.header').fadeToggle(200);
+        $('.video__popup').fadeToggle(200);
+
+        var video_data = $('.video__popup iframe').data('src');
+        $('.video__popup iframe').attr('src', video_data);
     });
     //hide
     $('.video__popup').click(function() {
-        $('.video__popup').toggle();
+        $('.video__popup').fadeToggle(200);
+        $('.header').fadeToggle(200);
         $('.intro__play_outline').toggleClass('active');
+
+        $('.video__popup iframe').attr('src', '');
+    });
+
+    // Height for gallery
+    function windowSize(){
+        if ($(window).width() >= '320'){
+          var imgWidth = $('.tent__gallery_item').width();
+          $('.tent__gallery_item').css({
+            'height' : imgWidth
+          });
+        } else if ($(window).width() <= '768'){
+    
+        }
+    }
+    
+    $(window).on('load resize',windowSize);
+
+    // Mask for input-phone
+    jQuery(function($){
+        $(".input__phone").mask("+7 (999) 999-99-99");
     });
 });
