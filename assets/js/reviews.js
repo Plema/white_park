70f24bd31d6e797bafc267f6acfa385cb6ee7b00
Anton Milasenko <48668353+Miant1@users.noreply.github.com>
2021-03-02 22:12:15 +0200

    last changes from 2.03

--- assets/js/reviews.js
+++ assets/js/reviews.js
@@ -100,4 +100,45 @@ $(document).ready(function() {
             },
         }
     });
+    
+    // Show/hide nav for another pages
+    function showMenuLine() {
+        $('.header__info_line').css({'right': '0px'});
+    }
+    function hideMenuLine() {
+        $('.nav__toggle_line').css({'right': '0px'});
+    }
+
+    $('.nav__toggle_line').on("click", function(e) {
+        e.preventDefault();
+
+        $(this).toggleClass('active');
+        if ($('.nav__toggle_line').hasClass('active')) {
+            $('.nav__toggle_btn').find('i').removeClass('icon-menu');
+            $('.nav__toggle_btn').find('i').addClass('icon-close');
+            $('.header__info_line').addClass('active');
+
+        } else {
+            $('.nav__toggle_btn').find('i').removeClass('icon-close');
+            $('.nav__toggle_btn').find('i').addClass('icon-menu');
+            $('.header__info_line').removeClass('active');
+        }
+        if($('.nav__toggle_line').hasClass('active')){
+            $('.nav__toggle_line').css({'right': '-50px'});
+
+            setTimeout(showMenuLine, 200);
+        }
+    });
+
+    $(document).click(function (e) {
+        var btn = $('.nav__toggle_line');
+        if (!btn.is(e.target) && btn.has(e.target).length === 0) {
+            $('.header__info_line').css({'right': '-300px'});
+            $('.nav__toggle_line').removeClass('active');
+            $('.nav__toggle_line .nav__toggle_btn').find('i').removeClass('icon-close');
+            $('.nav__toggle_line .nav__toggle_btn').find('i').addClass('icon-menu');
+
+            setTimeout(hideMenuLine, 300);
+        }
+    });
 });
